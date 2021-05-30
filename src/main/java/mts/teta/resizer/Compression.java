0bde90e65056bee749e458615d3ed81707096989
AleksandraKlua <alex.klua@gmail.com>
2021-05-30 23:15:32 +0300

    Add a private class for compressing an image

--- src/main/java/mts/teta/resizer/Compression.java
+++ src/main/java/mts/teta/resizer/Compression.java
@@ -9,29 +9,33 @@ import java.io.IOException;
 
 public class Compression {
     public void compress(String inputImagePath, String qualityValue) {
-        File inputImage = new File(inputImagePath);
         Checks compressCheck = new Checks();
         if (compressCheck.checkImagePath(inputImagePath)) {
             if (compressCheck.checkIfParameterIsNumber(qualityValue)) {
-                float qualityValueFloat = Float.parseFloat(qualityValue) / 100;
                 if (compressCheck.checkIfParameterIsAllowableSize(qualityValue, 0, 100)) {
-                    ImageParams imageParameter = new ImageParams();
-                    imageParameter.setImagePath(inputImagePath);
-                    imageParameter.setImageName(inputImagePath);
-                    imageParameter.setImagePostfixName(qualityValue);
-                    imageParameter.setImageFormat(inputImagePath);
-
-                    try {
-                        BufferedImage image = ImageIO.read(inputImage);
-                        Thumbnails.of(inputImage)
-                                .size(image.getWidth(), image.getHeight())
-                                .outputQuality(qualityValueFloat)
-                                .toFile(imageParameter.getNewFilePath());
-                    } catch (IOException e) {
-                        System.out.println("Smth went wrong");
-                    }
+                    getCompressedImage(inputImagePath, qualityValue);
                 }
             }
+        }
+    }
+
+    private void getCompressedImage(String inputImagePath, String qualityValue){
+        File inputImage = new File(inputImagePath);
+        ImageParams imageParameter = new ImageParams();
+        float qualityValueFloat = Float.parseFloat(qualityValue) / 100;
+        imageParameter.setImagePath(inputImagePath);
+        imageParameter.setImageName(inputImagePath);
+        imageParameter.setImagePostfixName(qualityValue);
+        imageParameter.setImageFormat(inputImagePath);
+
+        try {
+            BufferedImage image = ImageIO.read(inputImage);
+            Thumbnails.of(inputImage)
+                    .size(image.getWidth(), image.getHeight())
+                    .outputQuality(qualityValueFloat)
+                    .toFile(imageParameter.getNewFilePath());
+        } catch (IOException e) {
+            System.out.println("Smth went wrong");
         }
     }
 }
