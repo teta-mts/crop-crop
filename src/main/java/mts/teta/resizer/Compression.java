00fa5f1aa40fda80f855ddfc6dacc9f282dab0f0
AleksandraKlua <alex.klua@gmail.com>
2021-05-30 14:15:00 +0300

    Add compression function without checks

---
+++ src/main/java/mts/teta/resizer/Compression.java
@@ -0,0 +1,18 @@
+package mts.teta.resizer;
+
+import net.coobird.thumbnailator.Thumbnails;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Compression {
+    public void compress() throws IOException {
+        File inputImage = new File("1.jpg");
+        BufferedImage image = ImageIO.read(inputImage);
+        int width = image.getWidth();
+        int height = image.getHeight();
+        Thumbnails.of(new File("1.jpg")).size(width, height).outputQuality(0.5).toFile(new File("3.jpg"));
+    }
+}
